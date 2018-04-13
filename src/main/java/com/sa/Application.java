c99a8efdfe6b6ac349d577062ed4b998f4e94daf
xiaojinsb <22748280@qq.com>
2018-04-13 18:06:34 +0800

    初始项目

---
+++ src/main/java/com/sa/Application.java
@@ -0,0 +1,21 @@
+package com.sa;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 应用启动类
+ * @Description:   主模块
+ * @ClassName:     Application.java
+ * @author         moe
+ * @Date           2018-04-13
+ */
+
+@SpringBootApplication
+@MapperScan("com.sa.modules.dao")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
